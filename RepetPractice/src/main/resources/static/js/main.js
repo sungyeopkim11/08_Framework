f4ccd83aaf1c913b98daf4b5d86f61dc49bf7832
sungyeopkim11 <777sy777@naver.com>
2024-10-06 18:04:57 +0900

    20241006 세미 모달

--- RepetPractice/src/main/resources/static/js/main.js
+++ RepetPractice/src/main/resources/static/js/main.js
@@ -1,22 +1,20 @@
-// 모달과 a 태그 요소를 선택
-const modal = document.getElementById("loginModal");
-const link = document.getElementById("loginLink");
-const span = document.getElementsByClassName("close")[0];
+const login = document.querySelector("#login");
+const loginModal = document.querySelector("#loginModal");
+const close = document.querySelector(".close");
 
-// a 태그 클릭 시 모달을 열기
-link.onclick = function(event) {
-    event.preventDefault(); // 링크의 기본 동작을 막기 위해 사용합니다.
-    modal.style.display = "block"; // 모달을 화면에 보이게 설정합니다.
-}
+login.addEventListener("click", e => {
+    e.preventDefault();
+    loginModal.style.display = "block";
+});
 
-// 닫기 버튼 클릭 시 모달을 닫기
-span.onclick = function() {
-    modal.style.display = "none"; // 모달을 다시 감춥니다.
-}
+close.addEventListener("click", () => {
+    loginModal.style.display = "none";
+});
 
-// 모달 외부를 클릭했을 때 모달을 닫기
-window.onclick = function(event) {
-    if (event.target === modal) { // 클릭한 대상이 모달 배경이라면
-        modal.style.display = "none"; // 모달을 닫습니다.
+window.addEventListener("click", e => {
+    if(e.target === loginModal){
+       loginModal.style.display = "none";
     }
-}
+});
+
+
