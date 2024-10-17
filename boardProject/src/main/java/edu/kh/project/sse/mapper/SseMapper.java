bdac8722966df83c1c5f953a169c14c20b8ca9c8
sungyeopkim11 <777sy777@naver.com>
2024-10-17 17:51:10 +0900

    20241017 board 프로젝트 알림 기능 구현 세미 프로젝트 UI

---
+++ boardProject/src/main/java/edu/kh/project/sse/mapper/SseMapper.java
@@ -0,0 +1,31 @@
+package edu.kh.project.sse.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import edu.kh.project.sse.dto.Notification;
+
+@Mapper
+public interface SseMapper {
+
+	/** 알림 삽입
+	 * @param notification
+	 * @return result
+	 */
+	int insertNotification(Notification notification);
+
+	/** 알림을 받아야하는 회원의 번호 + 안읽은 알람 개수 조회
+	 * @param notificationNo
+	 * @return map
+	 */
+	Map<String, Object> selectReceiveMember(int notificationNo);
+
+	/** 로그인 한 회원의 알림 목록 조회
+	 * @param memberNo
+	 * @return
+	 */
+	List<Notification> selectNotificationList(int memberNo);
+
+}
